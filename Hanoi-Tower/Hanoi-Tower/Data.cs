ceb68767be5231638cd96c5bfee45526ffcd09b4
zickbalazs <balazs.zick2@gmail.com>
2022-09-28 08:18:24 +0200

    OOP

---
+++ Hanoi-Tower/Hanoi-Tower/Data.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanoi_Tower
+{
+    class Data
+    {
+        public static List<List<int>> Towers;
+        public Data(int discN, int towerToBeFilled)
+        {
+            Towers = new List<List<int>>()
+            {
+                new List<int>(),
+                new List<int>(),
+                new List<int>()
+            };
+            Towers[towerToBeFilled - 1] = FillTower(discN);
+        }
+
+        private List<int> FillTower(int discs)
+        {
+            List<int> temp = new List<int>();
+            for (int i = 0; i < discs; i++)
+                temp.Add(i);
+            return temp;
+        }
+    }
+
+}
--- Hanoi-Tower/Hanoi-Tower/Game.cs
+++ Hanoi-Tower/Hanoi-Tower/Game.cs
@@ -13,42 +13,11 @@ namespace Hanoi_Tower
     
     public partial class Game : Form
     {
-        static List<int[]> Ruds;
-        private static int tallers, begin, end;
+        private Data data;
         public Game(int korong, int from, int to)
         {
             InitializeComponent();
-            begin = from;
-            Ruds = new List<int[]>()
-            {
-                new int[tallers],
-                new int[tallers],
-                new int[tallers]
-            };
-            end = to;
-            tallers = korong;
-        }
-        private void ZoeloepEinfuellen()
-        {
-            switch (begin)
-            {
-                case 1:
-                    Fill(1);
-                    break;
-                case 2:
-                    Fill(2);
-                    break;
-                case 3:
-                    Fill(3);
-                    break;
-            }
-        }
-        private int[] Fill(int Ausgewaehlten_ZoeLoep)
-        {
-
-
-
-            return;
+            data = new Data(korong, from);
         }
     }
 }
